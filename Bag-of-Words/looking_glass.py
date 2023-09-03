32db328af77409dc7b2d7d5b2df4c4fd3ff09c1c
iMythms <maythams.149@gmail.com>
2023-09-03 05:52:54 +0300

    commit

---
+++ Bag-of-Words/looking_glass.py
@@ -0,0 +1,64 @@
+looking_glass_text = """
+Indeed, the mysterious object kept growing, taking on an increasingly human-like appearance. By the time she was just a few steps away, it was unmistakably an entity with facial features—eyes, a nose, a mouth. It could be none other than the infamous HUMPTY DUMPTY. That much was certain, as though his identity was written across his visage.
+
+His large face could easily have accommodated the name a hundredfold. Perched atop a towering, narrow wall with legs folded in a manner reminiscent of a yogi, Humpty Dumpty paid no attention to her. His gaze was fixated elsewhere, leading her to believe he might just be an inanimate sculpture.
+
+"What a remarkable resemblance to an egg," she commented, her hands poised to catch him, expecting him to topple at any moment.
+
+"Quite irritating to be constantly referred to as an egg," Humpty Dumpty finally spoke, without making eye contact.
+
+"I said you resembled an egg," she clarified softly, adding, "And some eggs are quite aesthetically pleasing," hoping to smooth over her earlier statement.
+
+"Sense is something that seems to elude certain individuals," he retorted, still not facing her.
+
+Left without a proper reply, she started muttering a nursery rhyme under her breath:
+
+    Humpty Dumpty perched on a ledge,
+    Humpty Dumpty teetered on the edge.
+    Neither King's cavalry nor King's men,
+    Could ever restore Humpty's position again.
+
+"The final line doesn't quite fit the metric structure," she almost said out loud, but then remembered he would hear her.
+
+"Stop mumbling to yourself and state your purpose," he demanded, finally acknowledging her presence.
+
+"My name is Alice," she started, but he cut her off.
+
+"An uninspiring name, to say the least," he interrupted. "Does it signify anything?"
+
+"Should a name signify something?" she questioned.
+
+"Absolutely," he snorted. "My name signifies my form, which is strikingly handsome, if I may say so. With a name like Alice, one could be anything."
+
+"Why are you sitting here all alone?" she queried, hoping to avoid a confrontation.
+
+"Because there's no one else here, obviously," he huffed, visibly annoyed by the question.
+
+"Wouldn't it be safer on the ground?" she ventured, worried about his precarious position.
+
+"Child's play, these questions of yours," he grumbled. "There's no chance I'd fall, but even if I did..." His expression turned so austere that she nearly burst into laughter. "Even if I did, the King would send his entire brigade to fetch me."
+
+"To fetch you with all his horses and all his men?" she interjected.
+
+"How dare you eavesdrop!" he roared. "You must have been spying to know that!"
+
+"Actually, it's common knowledge, found in a book," she reassured him.
+
+"Ah, history books," he muttered, calming down. "Very well, I'll let you shake hands with a creature who has conversed with a King. And don't worry about my mouth extending too far. My head won't come off."
+
+"Are you sure you'd be saved so quickly?" she went on.
+
+"Of course! But let's slow down, this conversation is getting ahead of itself."
+
+"I can't recall your last comment," she admitted.
+
+"Then we'll start anew," he concluded, "And now it's my turn to quiz you. State your age."
+
+She quickly did the math and responded, "Seven years and six months."
+
+"Incorrect," he boasted. "You never mentioned such a thing!"
+
+"You misunderstood," she sighed. "I thought you were asking about my age."
+
+"If I'd meant that, I would have said so," retorted Humpty Dumpty.
+"""
--- Bag-of-Words/script.py
+++ Bag-of-Words/script.py
@@ -1,15 +1,37 @@
-# importing regex and nltk
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-
-# importing Counter to get word counts for bag of words
-from collections import Counter
+# importing part-of-speech function for lemmatization
+from part_of_speech import get_part_of_speech
 
 # importing a passage from Through the Looking Glass
 from looking_glass import looking_glass_text
 
-# importing part-of-speech function for lemmatization
-from part_of_speech import get_part_of_speech
+# importing Counter to get word counts for bag of words
+from collections import Counter
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+import re
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# Change text to another string:
+text = looking_glass_text
+
+cleaned = re.sub('\W+', ' ', text).lower()
+tokenized = word_tokenize(cleaned)
+
+stopwords_list = stopwords.words('english')
+filtered = [word for word in tokenized if word not in stopwords_list]
+
+normalizer = WordNetLemmatizer()
+normalized = [normalizer.lemmatize(
+    token, get_part_of_speech(token)) for token in filtered]
+
+# Comment out the print statement below
+# print(normalized)
+
+# Defining bag_of_looking_glass_words & print:
+bag_of_looking_glass_words = Counter(normalized)
+print(bag_of_looking_glass_words)
